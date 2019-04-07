d8848ce9ff9efd629cb7ad7c16490fb8fab8dea7
goruma <47574958+goruma@users.noreply.github.com>
2019-04-07 10:27:26 -0400

    Update P5HW2_RockPaperScissors_AdrianGorum.py

--- P5HW2_RockPaperScissors_AdrianGorum.py
+++ P5HW2_RockPaperScissors_AdrianGorum.py
@@ -1,45 +1,65 @@
-# A brief description of the project
+# This program allows the user to play rock, paper, scissors with the computer. 
 # 4-4-2019
 # CTI-110 P5HW2 - Rock, Paper, Scissors Game
 # Adrian Gorum
 #
 
+#Imports random Module
 import random
 
+#Initialize Global Variables
+#Initialize minimum and maximum variables for a range to pick random numbers from.
 MIN = 1
 MAX = 3
+#Also initializes gameRestarts variable for determining how many times the game has been restarted
 gameRestarts = 0
 
+#Define main() fucntion which prints a different message to the user based on gameRestarts global variable, 
+#initializes the aiRandom variable which chooses between a range of MIN and MAX, and calls the other defined
+#functions to play the game.
 def main(gameRestarts):
     
     if gameRestarts == 0:
         print("LET'S PLAY ROCK, PAPER, SCISSORS HUMAN!!!\n")
     elif gameRestarts > 0:
         print("LET'S PLAY AGAIN!!!\n")
-        
+    
+    #Initialize aiRandom variable to store a random int between MIN and MAX
     aiRandom = random.randint(MIN,MAX)
+
+    #Initialize aiPick variable storing which decision is picked by the computer based on aiRandom variable
     aiPick = aiDecision(aiRandom)
-    playerPick = playerDecision()
+    #Initialize playerPick variable which stores the returned value from playerDecision() function    playerPick = playerDecision()
+
+    #Calls calculateChoices() function and passes aiPick and playerPick as arguments
     calculateChoices(aiPick,playerPick)
+    #Calls restart() function and passes gameRestarts variable as an argument.
     restart(gameRestarts)    
 
 
+#Define aiDecision() function which requires aiRandom argument. This function determines which random #number, between MIN and MAX is assigned to aiRandom and assigns either rockm, paper, or scissor string to #the aiChoice variable.
 def aiDecision(aiRandom):
+    #Initialize aiChoice variable
     aiChoice = "str"
 
+    #Determine which string to assign to aiChoice variable based on the random int assigned to aiRandom
     if aiRandom == 1:
         aiChoice = "rock"
     elif aiRandom == 2:
         aiChoice = "paper"
     elif aiRandom == 3:
         aiChoice = "scissors"
-
+    #return aiChoice variable
     return aiChoice
 
+#Define playerDecision() function which prompts user for input. User input is stored in playerChoice #variable. If user inputs rock, paper, or scissors return playerChoice. If user enters anything other than #those three string choices perform a while loop to prompt user to enter rock, paper, or scissors. If player #inputs either of the three approved string values, exit while loop and return playerChoice variable.
 def playerDecision():
+    #Initialize playerChoice variable and prompt user for input
     playerChoice = input('Enter your choice of "paper", "rock", or "scissors": ')
     print('')
     
+    #Determine if user inputted correct string values. If player enters anything other than the correct
+    #string values, continue to prompt user to enter the correct string values. Return playerChoice variable
     if playerChoice == "rock" or playerChoice == "paper" or playerChoice == "scissors":
         return playerChoice
     elif playerChoice != "rock" or playerChoice != "paper" or playerChoice!= "scissors":
@@ -53,6 +73,7 @@ def playerDecision():
                 exit()
         
 
+#Define calculateChoices() function. This function determines whether the computer’s choice or the player’s #choice equals a win condition. If the player’s choice is equal to the computer’s choice the game calls the #restart() function.
 def calculateChoices(aiPick,playerPick):
     if aiPick == "rock" and playerPick == "scissors":
         print('Computer Wins!!! Rock CRUSHES Scissors!\n')
@@ -86,6 +107,7 @@ def calculateChoices(aiPick,playerPick):
         print('Player picked: ', playerPick,'\n')
         restart(gameRestarts)
 
+#Define restart() function. This function prompts the user to input a choice of y or n to continue playing #the game or to quit the program. A while loop is used to continue prompting the user for the correct string #values.
 def restart(gameRestarts):
     start = 1
 
